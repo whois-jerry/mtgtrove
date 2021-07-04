d4fe26f3cac5c0edcf26524fe78e756b3d080194
whois-jerry <82949691+whois-jerry@users.noreply.github.com>
2021-07-04 09:05:42 +0800

    Trying: cp -R /assets /www

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ cd /tmp
 mkdir www
 cp index.html www
 cp script.js www
-cp /assets /www
+cp -R /assets /www
 echo "<hr>Running on $(hostname)" >> www/index.html
 cd www
 python -m SimpleHTTPServer 8080
